2c38c15a40437ba85a8656b341c169dd0bf26012
Anonymous <user@anon.co>
2024-01-29 14:32:42 +0100

    Add icons

--- resources/views/klanten/create.blade.php
+++ resources/views/klanten/create.blade.php
@@ -9,7 +9,77 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    Dit is de klanten pagina.
+                    <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
+                        <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">
+                            Algemene informatie
+                        </h2>
+                        <form action="#">
+                            <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
+                                <div class="sm:col-span-2">
+                                    <label for="email"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                        Email
+                                    </label>
+                                    <input type="email" name="email" id="email"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                        placeholder="Email van de klant" required>
+                                </div>
+                                <div class="w-full">
+                                    <label for="first_name"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                        Voornaam
+                                    </label>
+                                    <input type="text" name="first_name" id="first_name"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                        placeholder="Jan" required>
+                                </div>
+                                <div class="w-full">
+                                    <label for="last_name"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                        Achternaam
+                                    </label>
+                                    <input type="text" name="last_name" id="last_name"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                        placeholder="Janssen" required>
+                                </div>
+                                <div>
+                                    <label for="country"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Land</label>
+                                    <select id="country" name="country"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                        <option value="Nederland">Nederland</option>
+                                        <option value="België">België</option>
+                                        <option value="Duitsland">Duitsland</option>
+                                        <option value="Verenigd Koninkrijk">
+                                            Verenigd Koninkrijk
+                                        </option>
+                                        <option value="India">India</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <label for="phone"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                        Telefoonnummer
+                                    </label>
+
+                                    <input type="text" name="phone" id="phone"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                        placeholder="0612345678" required>
+                                </div>
+                                <div class="sm:col-span-2">
+                                    <label for="description"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+                                    <textarea id="description" rows="8"
+                                        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                        placeholder="Your description here"></textarea>
+                                </div>
+                            </div>
+                            <button type="submit"
+                                class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
+                                Klant toevoegen @svg('heroicon-o-arrow-right', 'w-4 h-4 ml-2')
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
