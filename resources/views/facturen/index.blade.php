7706b36516de2faf5ae7158253e15ee84fe8ee86
Marwan Fikri <marwan.fikri200@gmail.com>
2024-01-29 17:02:54 +0100

    last changes for today

--- resources/views/facturen/index.blade.php
+++ resources/views/facturen/index.blade.php
@@ -12,7 +12,7 @@
                     Dit is de factuur pagina.
 
                     <a href="{{ route('facturen.generate') }}"
-                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
                         Nieuwe factuur
                     </a>
                 </div>
