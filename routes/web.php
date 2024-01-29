2ffef1fcdc36fd4b5790e57c2cefd16d009f5a8f
Marwan Fikri <marwan.fikri200@gmail.com>
2024-01-29 12:50:01 +0100

    miauw

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
+
+
+Route::get('/', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
