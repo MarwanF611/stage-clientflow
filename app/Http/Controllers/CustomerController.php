55b09361359f9a07ba63c49cbe2d58a8bf03f906
Ray Orole <orole.ray@protonmail.com>
2024-01-30 10:08:34 +0100

    Modify product controller

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -26,31 +26,31 @@ class CustomerController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'voornaam' => 'required',
-            'achternaam' => 'required',
-            'email' => 'required',
-            'telefoonnummer' => 'required',
-            'straatnaam' => 'required',
-            'huisnummer' => 'required',
-            'postcode' => 'required',
-            'land' => 'required',
-            'bedrijfsnaam' => 'required',
-            'btw_nummer' => 'required',
-            'iban' => 'required',
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'phone_number' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:customers',
+            'street_name' => 'required|string|max:255',
+            'house_number' => 'required|string|max:255',
+            'postcode' => 'required|string|max:255',
+            'country' => 'required|string|max:255',
+            'company_name' => 'required|string|max:255',
+            'vat_number' => 'required|string|max:255',
+            'iban' => 'required|string|max:255',
         ]);
 
         try {
             $klant = new Klant();
-            $klant->voornaam = $request->voornaam;
-            $klant->achternaam = $request->achternaam;
+            $klant->first_name = $request->first_name;
+            $klant->last_name = $request->last_name;
             $klant->email = $request->email;
-            $klant->telefoonnummer = $request->telefoonnummer;
-            $klant->straatnaam = $request->straatnaam;
-            $klant->huisnummer = $request->huisnummer;
+            $klant->phone_number = $request->phone_number;
+            $klant->street_name = $request->street_name;
+            $klant->house_number = $request->house_number;
             $klant->postcode = $request->postcode;
-            $klant->land = $request->land;
-            $klant->bedrijfsnaam = $request->bedrijfsnaam;
-            $klant->btw_nummer = $request->btw_nummer;
+            $klant->country = $request->country;
+            $klant->company_name = $request->company_name;
+            $klant->vat_number = $request->vat_number;
             $klant->iban = $request->iban;
             $klant->save();
         } catch (\Exception $e) {
