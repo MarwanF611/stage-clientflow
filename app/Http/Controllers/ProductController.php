64011511f7f865b19447b85e4b4a986a93ed7421
Ray Orole <orole.ray@protonmail.com>
2024-01-30 13:02:03 +0100

    Image support

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+// Import UUID
+use Illuminate\Support\Str;
 
 class ProductController extends Controller
 {
@@ -28,14 +30,31 @@ class ProductController extends Controller
         $request->validate([
             'name'  => 'required',
             'type' => 'required',
-            'price' => 'required|numeric|min:0',
+            'price' => 'required|numeric|min:0|max:9999',
             'stock' => 'required|integer|min:0',
-            'image' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
 
-        dd($request->all());
+        // Get image file
+        $image = $request->file('image');
+        $new_filename = Str::uuid() . '.' . $image->getClientOriginalExtension();
 
-        return redirect()->route('product.index')
+        // Store image file on disk
+        $image->storeAs('public/images', $new_filename);
+
+        // Create product
+        $product = new Product([
+            'name'  => $request->get('name'),
+            'type' => $request->get('type'),
+            'price' => $request->get('price'),
+            'stock' => $request->get('stock'),
+            'image' => $new_filename,
+        ]);
+
+
+        $product->save();
+
+        return redirect()->route('products.index')
             ->with('success', 'product is succesvol aangemaakt.');
     }
 }
