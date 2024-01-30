ad579054a96ec174570fc71e17d6412b8e93af71
Ray Orole <orole.ray@protonmail.com>
2024-01-30 11:42:12 +0100

    Add countries seder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        //Seed the countries
+        $this->call(CountriesSeeder::class);
+        $this->command->info('Seeded the countries!');
     }
 }
